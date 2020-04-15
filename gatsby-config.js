4e13bf41454b2e478eb4310b563ea041990d2e2e
Tomasz Goral <tomasz.goral@boldare.com>
2020-04-15 09:18:36 +0200

    feat: manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Maska Polka`,
+        short_name: `maskapolka`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#cbe8ec`,
+        theme_color: `#cbe8ec`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
     {
