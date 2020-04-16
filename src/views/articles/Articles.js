55835d94896905f858b6a48b51c6ce76c77f4b22
Tomasz Goral <tomasz.goral@boldare.com>
2020-04-16 21:37:35 +0200

    fix: inner html css, articles show more

--- src/views/articles/Articles.js
+++ src/views/articles/Articles.js
@@ -1,11 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import "./Articles.scss"
 import { BlogPostType, YoutubeVideoType } from "~/types"
-import { ArticleTile, YoutubeTile } from "@components"
+import { ArticleTile, YoutubeTile, Button } from "@components"
 import { Link } from "gatsby"
 
+const YoutubeBox = ({ link, title }) => (
+  <a href={link} target="_blank" rel="noopener noreferrer" aria-label={title}>
+    <YoutubeTile url={link} title={title} />
+  </a>
+)
+
+const ArticleBox = ({ slug, title, content, heroImage }) => (
+  <Link to={`/blog/${slug}`} aria-label={title}>
+    <ArticleTile
+      title={title}
+      excerpt={content.childMarkdownRemark.excerpt}
+      image={heroImage}
+    />
+  </Link>
+)
+
 const Articles = ({ posts, videos }) => {
+  const items = [
+    ...posts.map(p => ({ ...p, type: "post" })),
+    ...videos.map(v => ({ ...v, type: "video" })),
+  ].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+  const [limit, setLimit] = useState(4)
+
   return (
     <section className="articles section" id="articles">
       <h2 className="heading-primary">Aktualności:</h2>
@@ -14,30 +36,25 @@ const Articles = ({ posts, videos }) => {
         tylko.
       </p>
       <ul className="articles__list">
-        {posts.map(post => (
-          <li className="articles__list-item" key={post.slug}>
-            <Link to={`/blog/${post.slug}`} aria-label={post.title}>
-              <ArticleTile
-                title={post.title}
-                excerpt={post.content.childMarkdownRemark.excerpt}
-                image={post.heroImage}
-              />
-            </Link>
-          </li>
-        ))}
-        {videos.map(video => (
-          <li className="articles__list-item" key={video.url}>
-            <a
-              href={video.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={video.title}
-            >
-              <YoutubeTile url={video.link} title={video.title} />
-            </a>
+        {items.slice(0, limit).map(item => (
+          <li className="articles__list-item" key={item.createdAt}>
+            {item.type === "post" ? (
+              <ArticleBox {...item} />
+            ) : (
+              <YoutubeBox {...item} />
+            )}
           </li>
         ))}
       </ul>
+      {limit < items.length && (
+        <Button
+          className="articles__show-more"
+          onClick={() => setLimit(limit + 4)}
+          color="white"
+        >
+          Pokaż więcej
+        </Button>
+      )}
     </section>
   )
 }
