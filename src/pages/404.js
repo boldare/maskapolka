42b59050a5d526be752c399c9b7d12b7b8050ca3
Tomasz Goral <tomasz.goral@boldare.com>
2020-04-17 07:41:42 +0200

    add 404 redirect to articles

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,11 @@ import React from "react"
 
 import { SEO } from "@components"
 
-const NotFoundPage = () => (
-  <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+const NotFoundPage = () => {
+  useEffect(() => {
+    navigate("/#articles")
+  }, [])
+  return null
+}
 
 export default NotFoundPage
